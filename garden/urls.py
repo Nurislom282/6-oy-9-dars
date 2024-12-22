3884f732fdebe474de1676acc196e167b36417f0
Nurislom <anurislom425@gmail.com>
2024-12-22 11:26:37 +0500

    11-dars ubdate tayyor

--- garden/urls.py
+++ garden/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index,gul_by_tur,gul_detail,add_post
+from .views import index,gul_by_tur,gul_detail,add_post,update_post
 
 urlpatterns = [
     path('', index, name='home'),
     path('Turlar/<int:turi_id>/', gul_by_tur, name='gul_by_tur'),
     path('posts/<int:pk>/', gul_detail, name='detail'),
     path('add-post/', add_post, name='add_post'),
+    path('posts/<int:pk>/update/', update_post, name='update_post'),
+
 ]
--- garden/views.py
+++ garden/views.py
@@ -44,3 +44,33 @@ def add_post(request: WSGIRequest):
         "form": form
     }
     return render(request, 'add_post.html', context)
+
+def update_post(request: WSGIRequest, pk: int):
+    post = get_object_or_404(Gul, pk=pk)
+
+    if request.method == 'POST':
+        form = GulForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            post.name = form.cleaned_data.get("name")
+            post.content = form.cleaned_data.get("info")
+            post.photo = form.cleaned_data.get("photo") if form.cleaned_data.get("photo") else post.photo
+            post.category = form.cleaned_data.get("turi")
+            post.published = form.cleaned_data.get("published")
+            post.video = form.cleaned_data.get("video") if form.cleaned_data.get("video") else post.video
+            post.save()
+            return redirect('detail', pk=post.pk)
+
+    form = GulForm(initial={
+        "name": post.name,
+        "info": post.info,
+        "photo": post.photo,
+        "turi": post.turi,
+        "published": post.published
+    })
+
+    context = {
+        "form": form,
+        'post': post
+    }
+
+    return render(request, 'add_post.html', context)
